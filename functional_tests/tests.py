c2e0004a05616bd78b03a8431cb2109e2d8d6ad2
G. Troy Johnson <github@weave.sh>
2022-04-03 14:41:24 -0700

    make urls an include from lists app, clone contents from book_examples to make diff less noisy

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,20 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 MAX_WAIT = 10
 
+
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -25,14 +27,15 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-                time.sleep(0.25)
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
 
-        # she notices the page title and header mention to-do lists
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -44,36 +47,31 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
 
-        # she types "Buy peacock feathers" into a text box (Edith's hobby
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peackco feathers" as an item in the to-do list
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now shows both items in her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        #self.fail("Finish the test")
+        # Satisfied, she goes back to sleep
 
-        # She visits that URL - her to-do list is still there.
-
-        # Satisified, she goes back to sleep
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith tarts a new to-do list
+        # Edith start a new todo list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
@@ -87,11 +85,12 @@ class NewVisitorTest(LiveServerTestCase):
         # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
-        ## of Edith's is comint through from cookies, etc
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # Francis visits the home page. There is no sigh of Edith's list
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
@@ -110,9 +109,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # Satisified, they both go back to sleep
+        # Satisfied, they both go back to sleep
 
