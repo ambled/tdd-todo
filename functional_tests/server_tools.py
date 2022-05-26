a3a56a3bd59d6991cd23ec9322da95217549a840
G. Troy Johnson <github@weave.sh>
2022-05-25 21:06:06 -0700

    chapter 23 purist unit tests

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,5 +1,5 @@
 from fabric.api import run
-from fabric.context_managers import settings, shell_env
+from fabric.context_managers import settings
 
 
 def _get_manage_dot_py(host):
@@ -20,7 +20,6 @@ def _get_server_env_vars(host):
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'ubuntu@{host}'):
-        env_vars = _get_server_env_vars(host)
-        with shell_env(**env_vars):
-            session_key = run(f'{manage_dot_py} create_session {email}')
-            return session_key.strip()
+        session_key = run(f'{manage_dot_py} create_session {email}')
+        return session_key.strip()
+
